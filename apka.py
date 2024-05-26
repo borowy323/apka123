a05c2a6fe78cfda479e9c2c6ace2c5d2758873e4
borowy323 <121823782+borowy323@users.noreply.github.com>
2024-05-26 11:40:05 +0200

    nowa apkaa imie

--- apka.py
+++ apka.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
+# Strona główna z formularzem
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Cześć, tu Zoja hello !"
+    if request.method == 'POST':
+        name = request.form['name']
+        return render_template_string('''
+            <!doctype html>
+            <html>
+                <head>
+                    <title>Powitanie</title>
+                </head>
+                <body>
+                    <h1>Cześć, tu Zoja! Witaj, {{ name }}!</h1>
+                    <a href="/">Wróć</a>
+                </body>
+            </html>
+        ''', name=name)
+    return '''
+        <!doctype html>
+        <html>
+            <head>
+                <title>Powitanie</title>
+            </head>
+            <body>
+                <form method="post" action="/">
+                    <label for="name">Wpisz swoje imię:</label>
+                    <input type="text" id="name" name="name" required>
+                    <button type="submit">OK</button>
+                </form>
+            </body>
+        </html>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
